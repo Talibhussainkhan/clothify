b4f22b8a111e25d1abfa4fbb70c42e32ca776ad5
Talib Hussain Khan <talibhk161@gmail.com>
2025-09-28 16:44:29 +0500

    About page added

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,15 @@
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="max-w-7xl 2xl:max-w-[1500px] mx-auto px-5 sm:px-3">
+        <h1 className="text-3xl sm:text-5xl mt-7 mb-5 font-bold">About <span className="text-slate-500">Clothify</span></h1>
+        <div className="max-w-4xl">
+        <p className="text-sm sm:text-lg mb-3">At <span className="font-bold">Clothify</span>, we believe fashion is more than just clothing — it’s self-expression. Our mission is to bring you trendy, high-quality, and affordable outfits that make you look and feel confident every day.</p>
+        <p className=" text-sm sm:text-lg mb-3">From casual wear to elegant styles, <span className="font-bold">Clothify</span> offers a wide range of collections carefully designed to match your personality and lifestyle. We’re passionate about blending comfort with modern fashion, so you never have to compromise on style or quality.</p>
+        <p className="text-sm sm:text-lg mb-3">With a commitment to excellent customer service and a seamless shopping experience, Clothify isn’t just a shop — it’s your personal style partner.</p>
+        </div>
+        <h1 className="text-xl sm:text-2xl font-medium mb-3"><span className="text-slate-500">Clothify</span> — Wear Your Confidence.</h1>
+    </div>
   )
 }
 
