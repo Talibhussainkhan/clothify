fd3c66e4f23e135183c98373eb11742f49fe6caf
Talib Hussain Khan <talibhk161@gmail.com>
2025-09-28 15:06:18 +0500

    add react-router , create pages of client side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import Product from './pages/Product';
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Routes>
+
+      {/* client Route */}
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/cart' element={<Cart />} />
+      <Route path='/checkout' element={<Checkout />} />
+      <Route path='/product' element={<Product />} />
+    </Routes>
+    </>
   )
 }
 
