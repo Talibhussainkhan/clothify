e9e3e28e0d7f320a6fa563d8d4ce6c17ae094a54
Talib Hussain Khan <talibhk161@gmail.com>
2025-09-30 21:18:24 +0500

    added multer and cloudinary config

---
+++ api/config/multer.js
@@ -0,0 +1,27 @@
+import multer from "multer";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+
+
+cloudinary.config({
+    cloud_name : process.env,CLOUDINARY_CLOUD_NAME,
+    api_key : process.env.CLOUDINARY_API_KEY,
+    api_secret : process.env.CLOUDINARY_API_SECRET
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "uploads",
+    allowed_formats: ["jpg", "png", "jpeg", "webp"],
+     transformation: [
+      {
+        width: 1280,   // max width 1280px
+        quality: "auto:low", // auto compression (Cloudinary AI)
+        fetch_format: "webp", // webp usually smallest
+      },
+    ],
+  },
+});
+
+export const upload = multer({ storage });
