670e5eef85747a96dae8387304e9d575fe4ac59c
Talib Hussain Khan <talibhk161@gmail.com>
2025-09-30 19:14:45 +0500

    connect mongoDB database

--- api/index.js
+++ api/index.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import 'dotenv/config';
+import authRouter from './routes/adminAuth.js';
+import { connectDB } from './config/db.js';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+connectDB();
+
 // Middleware
 app.use(express.json());
-
-
+app.use('/api/auth', authRouter);
 
 
 app.listen(port, ()=>{
