681a8ac139fdb3afde45025ab9a02748d0daebb1
Talib Hussain Khan <talibhk161@gmail.com>
2025-10-01 20:51:20 +0500

    save state is LS with help of redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,22 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx'
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
-import { Provider } from 'react-redux'
-import { store } from './store/store.js';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "react-redux";
+import { persistor, store } from "./store/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
-    <BrowserRouter>
-    <App />
-   </BrowserRouter>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </PersistGate>
       </Provider>
     </QueryClientProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,8 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit'
-import  adminReducer  from './AuthSlice/adminAuth'
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import  adminReducer  from './AuthSlice/adminAuth';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import persistStore from 'redux-persist/es/persistStore';
+
+const rootReducer = combineReducers({ admin : adminReducer });
+
+const rootPersistConfig = {
+    key: 'root',
+    storage: storage,
+    version : 1
+}
+
+const persistedReducer = persistReducer(rootPersistConfig, rootReducer) 
+
 
 export const store = configureStore({
-  reducer: {
-    admin : adminReducer
-  },
+  reducer: persistedReducer,
+  // not show any error on browser
+  middleware : (getDefaultMiddleware)=>
+    getDefaultMiddleware({
+        serializableCheck : false,
+    }),
 })
+
+export const persistor = persistStore(store);
