eba886c939fa2d0045d169782e96c8acb8d72127
Talib Hussain Khan <talibhk161@gmail.com>
2025-10-02 16:09:36 +0500

    create delete route

--- api/routes/productRoutes.js
+++ api/routes/productRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { addProduct, getAllProductForAdmin } from '../controllers/productController.js';
+import { addProduct, getAllProductForAdmin, getDeleteProductById } from '../controllers/productController.js';
 import { upload } from '../config/multer.js';
 
 const productRouter = express.Router();
 
 productRouter.post('/create', upload.array('images') ,addProduct);
 productRouter.get("/get", getAllProductForAdmin);
+productRouter.delete("/delete", getDeleteProductById);
 
 
 
